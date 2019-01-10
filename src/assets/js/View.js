13c858125ddda882ae8fe390fe2b2fcf0bdf15d7
Leandro Costa <leandrohernandes95@gmail.com>
2019-01-10 13:05:22 -0200

    create view component.

--- src/assets/js/View.js
+++ src/assets/js/View.js
@@ -1,6 +1,26 @@
+const APP_MODES = ["list", "create", "edit"];
+
 class View {
-  constructor(template) {
+  constructor(template, emitter) {
     this.template = template;
+    this.emitter = emitter;
+    this.appMode = "list";
+
+    this.emitter.addListener("change_app_mode", this.setAppMode);
+  }
+
+  setAppMode = mode => {
+    if (APP_MODES.indexOf(mode) === -1) {
+      throw Error("invalid mode!");
+    }
+
+    this.appMode = mode;
+  };
+
+  render(users, parameters) {
+    const templ = this.template[this.appMode](users, parameters);
+
+    return templ;
   }
 }
 
